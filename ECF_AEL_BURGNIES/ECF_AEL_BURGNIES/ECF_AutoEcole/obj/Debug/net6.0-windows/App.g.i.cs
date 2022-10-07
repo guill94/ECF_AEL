7a19655d681e2e871682b6aa09e6c0121876642f
guill94 <86225123+guill94@users.noreply.github.com>
2022-10-07 09:37:40 +0200

    ECF2

--- ECF_AEL_BURGNIES/ECF_AEL_BURGNIES/ECF_AutoEcole/obj/Debug/net6.0-windows/App.g.i.cs
+++ ECF_AEL_BURGNIES/ECF_AEL_BURGNIES/ECF_AutoEcole/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ECF_AutoEcole {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ECF_AutoEcole {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public static void Main() {
             ECF_AutoEcole.App app = new ECF_AutoEcole.App();
             app.InitializeComponent();
